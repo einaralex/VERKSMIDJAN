2301525baffa84561691a9fb1e66d82152412840
einaralex <einaralex@gmail.com>
2025-04-11 09:13:10 +0000

    909

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,23 +2,22 @@ import styles from "./sidebar.module.css";
 import Cogwheels from "./Cogwheels";
 import Image from "next/image";
 export default function Sidebar() {
+  const audio = new Audio("/909.mp3");
+  const play = () => {
+    audio.play();
+  };
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarContent}>
-        <div className={styles.sidebarLogo}>
+        <button className={styles.sidebarLogo} onClick={play}>
           <Image
             src="/verksmidjan-logo-black-transparent.png"
             alt="Verksmidjan Logo"
             width={50}
             height={50}
           />
-        </div>
-        {/* <h1 className={styles.sidebarTitle}>V</h1> */}
-        {/* <nav>
-          <ul className={styles.sidebarNav}>
-            <li className={styles.sidebarNavItem}></li>
-          </ul>
-        </nav> */}
+        </button>
+
         <div className={styles.cogwheelsContainer}>
           <div className={styles.cogwheels}>
             <Cogwheels />
