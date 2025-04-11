d98e171511c88fddfacc9aed0b9c30f90988fb69
einaralex <einaralex@gmail.com>
2025-04-11 09:59:19 +0000

    cleanup

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import styles from "./sidebar.module.css";
 import Cogwheels from "./Cogwheels";
 import Image from "next/image";
+
 export default function Sidebar() {
-  const audio = new Audio("/909.mp3");
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    // This code will only run on the client side
+    setAudio(new Audio("/909.mp3"));
+  }, []);
+
   const play = () => {
-    audio.play();
+    if (audio) {
+      audio.play();
+    }
   };
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarContent}>
