a73685abad14aff21c5354aad2f03ff6714f993f
einaralex <einaralex@gmail.com>
2025-11-17 21:00:26 +0000

    chore: add link to utikottur streams

--- src/components/Releases/index.tsx
+++ src/components/Releases/index.tsx
@@ -1,6 +1,7 @@
 import CollapsedWrap from "../Rows/CollapsedWrap";
 import ExpandedWrap from "../Rows/ExpandedWrap";
-
+import Image from "next/image";
+import styles from "./releases.module.css";
 export function ReleasesCollapsed() {
   return <CollapsedWrap>Releases</CollapsedWrap>;
 }
@@ -10,7 +11,7 @@ export function ReleasesExpanded() {
     <>
       <ReleasesCollapsed />
       <ExpandedWrap>
-        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
           <iframe
             style={{ border: "0", width: "350px", height: "687px" }}
             src="https://bandcamp.com/EmbeddedPlayer/album=3132847855/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/campaign=verk/"
@@ -20,6 +21,54 @@ export function ReleasesExpanded() {
               Gæðablóð &amp; Bestaskinn by Útiköttur
             </a>
           </iframe>
+          <div className={styles.streamButtons}>
+            <a
+              href="https://soundcloud.com/verksmidjan/sets/gaedablod-og-bestaskinn"
+              target="_blank"
+            >
+              <button>
+                <Image
+                  src="/icons/soundcloud.png"
+                  alt="SoundCloud"
+                  width={24}
+                  height={24}
+                />
+                <span>SoundCloud</span>
+              </button>
+            </a>
+            <a
+              href="https://music.apple.com/us/artist/utik%C3%B6ttur/1853193367"
+              target="_blank"
+            >
+              <button>
+                <Image
+                  src="/icons/stream-apple.svg"
+                  alt="Apple Music"
+                  width={24}
+                  height={24}
+                />
+                <span>Apple Music</span>
+              </button>
+            </a>
+            <button>
+              <Image
+                src="/icons/stream-tidal.svg"
+                alt="Tidal"
+                width={24}
+                height={24}
+              />
+              <span>Tidal (soon)</span>
+            </button>
+            <button>
+              <Image
+                src="/icons/stream-spotify.svg"
+                alt="Spotify"
+                width={24}
+                height={24}
+              />
+              <span>Spotify (soon)</span>
+            </button>
+          </div>
         </div>
       </ExpandedWrap>
     </>
