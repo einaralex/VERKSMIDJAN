833aae97c6875915077a4e5417b7f0a3f4e466a3
einaralex <einaralex@gmail.com>
2026-01-03 21:43:10 +0000

    chore: update spotify and tidal links to utikottur

--- src/components/Releases/index.tsx
+++ src/components/Releases/index.tsx
@@ -11,10 +11,16 @@ export function ReleasesExpanded() {
     <>
       <ReleasesCollapsed />
       <ExpandedWrap>
-        <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
           <iframe
-            style={{ border: "0", width: "350px", height: "687px" }}
-            src="https://bandcamp.com/EmbeddedPlayer/album=3132847855/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/campaign=verk/"
+            style={{ border: "0", width: "600px", height: "373px" }}
+            src="https://bandcamp.com/EmbeddedPlayer/album=3132847855/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/campaign=verk/"
             seamless
           >
             <a href="https://utikottur.bandcamp.com/album/g-abl-bestaskinn">
@@ -50,24 +56,31 @@ export function ReleasesExpanded() {
                 <span>Apple Music</span>
               </button>
             </a>
-            <button>
-              <Image
-                src="/icons/stream-tidal.svg"
-                alt="Tidal"
-                width={24}
-                height={24}
-              />
-              <span>Tidal (soon)</span>
-            </button>
-            <button>
-              <Image
-                src="/icons/stream-spotify.svg"
-                alt="Spotify"
-                width={24}
-                height={24}
-              />
-              <span>Spotify (soon)</span>
-            </button>
+            <a href="https://tidal.com/album/474154918" target="_blank">
+              <button>
+                <Image
+                  src="/icons/stream-tidal.svg"
+                  alt="Tidal"
+                  width={24}
+                  height={24}
+                />
+                <span>Tidal</span>
+              </button>
+            </a>
+            <a
+              href="https://open.spotify.com/album/6ZrD39qY2mRnLGjzWM3vus?si=69957024ea674a57"
+              target="_blank"
+            >
+              <button>
+                <Image
+                  src="/icons/stream-spotify.svg"
+                  alt="Spotify"
+                  width={24}
+                  height={24}
+                />
+                <span>Spotify</span>
+              </button>
+            </a>
           </div>
         </div>
       </ExpandedWrap>
